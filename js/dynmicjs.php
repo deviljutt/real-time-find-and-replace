1f98b8071d872d24140325c7e29565bf076a9880
Nabeel Goraya <itsgoraya@gmail.com>
2020-02-26 14:38:44 +0500

    update funtion

--- js/dynmicjs.php
+++ js/dynmicjs.php
@@ -7,20 +7,27 @@ $pages = get_pages($args);
 ?>	
 <script>
 jQuery(document).ready(function($) {
-    var counter = 0;
+    var counter = jQuery('#id').val();
 
     $("#addrow").on("click", function () {
         var newRow = $("<tr>");
         var cols = "";
-
-        cols += '<td><textarea  class="form-control" name="search' + counter + '"/></textarea></td>';
-        cols += '<td><textarea  class="form-control" name="replace' + counter + '"/></textarea></td>';
-        cols += '<td><select name="page"><?php foreach ($pages as $page) {$title = $page->post_title;$id = $page->ID; echo '<option value='.$id.'>'.$title. "</option>";} ?>';
+		var inc=0;
+		var inc = inc+1;
+		
+		
+		
+		
+        cols += '<td><textarea  class="form-control" name="search[' + counter + ']"/></textarea></td>';
+        cols += '<td><textarea  class="form-control" name="replace[' + counter + ']"/></textarea></td>';
+        cols += '<td><select name="page[' + counter + ']"><option value="allpage">All Page</option><?php foreach ($pages as $page) {$title = $page->post_title;$id = $page->ID; echo '<option value='.$id.'>'.$title. "</option>";} ?>';
+		cols += '<input type="hidden" name="row' + counter + '">';
 
         cols += '<td><input type="button" class="ibtnDel btn btn-md btn-danger "  value="Delete"></td>';
         newRow.append(cols);
         $("table.order-list").append(newRow);
-        counter++;
+		counter = (counter - 1) + 2;
+		document.getElementById("id").value = counter;
     });
     $("table.order-list").on("click", ".ibtnDel", function (event) {
         $(this).closest("tr").remove();       
--- real-time-find-and-replace.php
+++ real-time-find-and-replace.php
@@ -36,34 +36,63 @@ include( plugin_dir_path( __FILE__ ) . 'js/dynmicjs.php');
 	<br><br>
 			
 			<?php 
-			
+			$i = 0;
 			$far_settings = get_option( 'far_plugin_settings' ); 
 
+			
+			
+	foreach ( $far_settings["search"] as $key => $find ){
+		
+		echo "</br></br>";
+		print_r($far_settings);
+		echo "</br></br>";
 
-	foreach ( $far_settings['setup-update'] as $key => $find ){
+echo "<div class='findif' id='id'>44</div>";
+
+		$searchfield = $far_settings["search"][$i];
+		$replacefield = $far_settings["replace"][$i];
+		$selectpage = $far_settings["page"][$i];
+		var_dump($selectpage);		
+
+				
+		$cols = '<tr ><td><textarea  class="form-control" name="search" />'.$searchfield.'</textarea></td>';
+        $cols.= '<td><textarea  class="form-control" name="replace"/>'.$replacefield.'</textarea></td>';			
+	   $cols.= '
+
+	   <td>
+	   
+	   <select name="page'.$i.'">
+	   
+	   <option value="1">All Page</option>
+	   <option value="2">1</option>
+	   <option value="3">2</option>
+	   
+	   </select>';
 		
-		echo "asdasd";
+		$cols .= '<td><input type="button" class="ibtnDel btn btn-md btn-danger "  value="Delete"></td>';
+
 		
-	}
+			echo $cols	;
+				
+				
+				
+				
+				
+				
+		$i = $i + 1;
+		echo "</tr>";
+			}
 	
-	
-	
-					
 				?>
 	
 	<br><br><br>
-	
-	
-	
-	
-	
-	
-     
-					<input type="hidden" name="setup-update" />
+
     </tbody>
     <tfoot>
         <tr>
             <td colspan="5" style="text-align: left;">
+				<input type="hidden" name="row">
+
                 <input type="button" class="btn btn-lg btn-block " id="addrow" value="Add Row" />
 				<input type="submit"/>
             </td>
@@ -79,12 +108,11 @@ include( plugin_dir_path( __FILE__ ) . 'js/dynmicjs.php');
 
 <?php 
 
-if ( isset( $_POST['setup-update'] ) ) {
-		$_POST = stripslashes_deep( $_POST );
+if ( isset( $_POST['row'] ) ) {
 		update_option( 'far_plugin_settings', $_POST );		
 }
 
-var_dump(get_option( 'far_plugin_settings' ));
+//var_dump(get_option( 'far_plugin_settings' ));
 
 
 
